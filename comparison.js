31d64ddcfca82c0f0a6b57d859d4fa4e74590098
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-28 02:29:12 +0600

    js-conditionals

---
+++ comparison.js
@@ -0,0 +1,13 @@
+/**
+ * Comparison
+ * 1. Bigger >
+ * 2. Less <
+ * 3. Equal ==
+ * 4. Greater Than or Equal >=
+ * 5. Less than or Equal = <=
+ * 6. Not Equal !=
+ * 
+ * -------------------------------
+ * 7.and (&&)
+ * 8. || (OR)
+ */
