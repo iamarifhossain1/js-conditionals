12b1c95a41db0eca6ee41111bcc141409e8b5d2c
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-28 12:46:40 +0600

    comparison.js

--- comparison.js
+++ comparison.js
@@ -11,3 +11,13 @@
  * 7.and (&&)
  * 8. || (OR)
  */
+
+// console.log (5 > 10);
+// console.log (5 < 10);
+// console.log (5 == 10);
+// console.log (10 == 10);
+// console.log (10 >= 10);
+// console.log (5 <= 10);
+// console.log (10 <= 10);
+// console.log (100 <= 10);
+console.log (100 <= 10);
