5a33daecca4216f03c857756acc691fb724ce73f
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-28 13:50:30 +0600

    multiple-conditions

---
+++ multiple-condition.js
@@ -0,0 +1,44 @@
+const salary = 24000;
+const isBCS = true;
+const height = 61;
+const hasCar = false;
+
+
+
+// if (salary > 20000 && height > 66) {
+//     console.log ('Laga Biyaaaa')
+// }
+
+// else { 
+//     console.log ('Onno Patro Khuji');
+// }
+
+
+// if (salary > 25000 || height > 66) {
+//     console.log ('Kobuuuuuuuuuul')
+// }
+
+// else { 
+//     console.log ('Vaaaaag tui mokbuuuuuuuuul');
+// }
+
+
+// more and more conditions
+
+// if (salary > 25000 || height > 66 || isBCS == true) {
+//     console.log ('Kobuuuuuuuuuul')
+// }
+
+// else { 
+//     console.log ('Vaaaaag tui mokbuuuuuuuuul');
+// }
+
+// ---------------Complex Condition----------------------
+
+if ((salary > 25000 && hasCar == true) || isBCS == true) { 
+    console.log ('Laga Biyaaaaaaaaaaaaaaaaaaaa');
+}
+
+else { 
+    console.log ('Biyaaaaaaaaaaaaaaai kormu nah')
+}
