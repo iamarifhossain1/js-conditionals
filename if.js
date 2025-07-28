728f37d045a8fec67c74dcefc2122c02e4abac15
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-28 13:33:23 +0600

    if-else condition

---
+++ if.js
@@ -0,0 +1,28 @@
+var biriyaniPrice = 300;
+if (biriyaniPrice <= 300) {
+    console.log ('Give me a packet of a Biriyani');
+}
+
+else {
+    console.log ('No Thanks, i dont need  it');
+}
+
+/**
+ * if (condition) {
+ * execute code if condtion above is true
+ * }
+ * 
+ * else {
+ * execute code if the condition is false
+ * }
+ */
+
+var weight = 20;
+
+if (weight <= 20) {
+    console.log ('i will carry it by myself');
+}
+
+else {
+    console.log ('i will rent a rickshaw');
+}
