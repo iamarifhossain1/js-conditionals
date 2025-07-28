7a2c319b81d6f09bd6645f7e21d3041b12c2f4f2
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-28 13:00:39 +0600

    comparison.js

--- comparison.js
+++ comparison.js
@@ -20,4 +20,11 @@
 // console.log (5 <= 10);
 // console.log (10 <= 10);
 // console.log (100 <= 10);
-console.log (100 <= 10);
+// console.log (10 == 10.1);
+
+// ..............................
+
+var guavaPrice = 40;
+var applePrice = 100;
+
+console.log (applePrice == guavaPrice);
